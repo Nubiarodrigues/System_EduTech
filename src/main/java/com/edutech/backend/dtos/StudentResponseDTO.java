ed03b989b8624b83ee02da0b84da71102b2e3bbd
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-06-09 13:55:30 -0300

    feat: criar dtos iniciais para a entidade Student

---
+++ src/main/java/com/edutech/backend/dtos/StudentResponseDTO.java
@@ -0,0 +1,26 @@
+package com.edutech.backend.dtos;
+
+import java.time.LocalDate;
+
+import com.edutech.backend.entities.Student;
+
+import jakarta.validation.constraints.NotNull;
+
+public record StudentResponseDTO(
+		Long id,
+		@NotNull String name,
+		LocalDate dateBirth,
+		@NotNull String cpf,
+		String motherName,
+		String fatherName) {
+
+	public StudentResponseDTO(Student student) {
+		this(
+				student.getId(),
+				student.getName(),
+				student.getDateBirth(), 
+				student.getCpf(), 
+				student.getMotherName(),
+				student.getFatherName());
+	}
+}
