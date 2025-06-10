ca6991212a67ec0bda3bcf8fae906ffe82b1dd44
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-06-10 15:39:36 -0300

    feat: criar camada de recurso da entidade Student, endpoint de delete

--- src/main/java/com/edutech/backend/controllers/StudentController.java
+++ src/main/java/com/edutech/backend/controllers/StudentController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,6 +61,12 @@ public class StudentController {
 		Student current = serviceStudent.updateStudent(id, dto);
 		StudentResponseDTO response = new StudentResponseDTO(current);
 		return ResponseEntity.ok(response);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		serviceStudent.deleteStudent(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
