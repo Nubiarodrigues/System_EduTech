4fca34811a8e72254de63a87a12da7a5117dd825
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-06-12 10:01:51 -0300

    feat(teacher): criar camada de persistência e carga de dados no banco

--- src/main/java/com/edutech/backend/config/TestConfig.java
+++ src/main/java/com/edutech/backend/config/TestConfig.java
@@ -9,10 +9,13 @@ import org.springframework.context.annotation.Profile;
 
 import com.edutech.backend.entities.Classroom;
 import com.edutech.backend.entities.Student;
+import com.edutech.backend.entities.Teacher;
 import com.edutech.backend.enuns.Shift;
+import com.edutech.backend.enuns.Situation;
 import com.edutech.backend.enuns.TeachingState;
 import com.edutech.backend.repositories.ClassroomRepository;
 import com.edutech.backend.repositories.StudentRepository;
+import com.edutech.backend.repositories.TeacherRepository;
 
 @Configuration
 @Profile("test")
@@ -20,10 +23,12 @@ public class TestConfig implements CommandLineRunner {
 
 	private final ClassroomRepository repositoryClassroom;
 	private final StudentRepository repositoryStudent;
+	private final TeacherRepository repositoryTeacher;
 
-	public TestConfig(ClassroomRepository repositoryClassroom, StudentRepository repositoryStudent) {
+	public TestConfig(ClassroomRepository repositoryClassroom, StudentRepository repositoryStudent, TeacherRepository repositoryTeacher) {
 		this.repositoryClassroom = repositoryClassroom;
 		this.repositoryStudent = repositoryStudent;
+		this.repositoryTeacher = repositoryTeacher;
 	}
 
 	@Override
@@ -48,6 +53,60 @@ public class TestConfig implements CommandLineRunner {
 		Student s9 = new Student("Thiago Martins", LocalDate.parse("2005-11-20"), "999.000.111-22", "9990001112233", "Rua I, 357", "839555-0009", "thiago.resp@gmail.com", "Cláudia Martins", "Paulo Martins", c3);
 		
 		repositoryStudent.saveAll(Arrays.asList(s1,s2,s3,s4,s5,s6,s7,s8,s9));
+		
+		Teacher t1 = Teacher.builder()
+				.name("José da Silva Morais")
+				.cpf("174.524.556-55")
+				.telephone("83598542145")
+				.status(Situation.ATIVO)
+				.dateBirth(LocalDate.parse("2002-01-22"))
+				.workloadTotal(30)
+				.rg("222222")
+				.address("Rua Maria das Graças, 81 - João Pessoa/PB")
+				.matriculation("145876")
+				.formedCourse("Licenciatura em Língua Portuguesa")
+				.build();
+
+			Teacher t2 = Teacher.builder()
+				.name("Ana Carolina Ferreira")
+				.cpf("123.456.789-00")
+				.telephone("83999887766")
+				.status(Situation.ATIVO)
+				.dateBirth(LocalDate.parse("1985-04-10"))
+				.workloadTotal(40)
+				.rg("123123")
+				.address("Av. Epitácio Pessoa, 1000 - João Pessoa/PB")
+				.matriculation("145877")
+				.formedCourse("Licenciatura em Matemática")
+				.build();
+
+			Teacher t3 = Teacher.builder()
+				.name("Marcos Paulo de Almeida")
+				.cpf("321.654.987-11")
+				.telephone("83996543210")
+				.status(Situation.ATIVO)
+				.dateBirth(LocalDate.parse("1990-09-15"))
+				.workloadTotal(20)
+				.rg("456456")
+				.address("Rua das Acácias, 345 - Campina Grande/PB")
+				.matriculation("145878")
+				.formedCourse("Licenciatura em História")
+				.build();
+
+			Teacher t4 = Teacher.builder()
+				.name("Fernanda Lopes Andrade")
+				.cpf("987.654.321-99")
+				.telephone("83991234567")
+				.status(Situation.ATIVO)
+				.dateBirth(LocalDate.parse("1995-12-05"))
+				.workloadTotal(25)
+				.rg("789789")
+				.address("Rua do Sol, 789 - Sousa/PB")
+				.matriculation("145879")
+				.formedCourse("Licenciatura em Ciências Biológicas")
+				.build();
+			
+			repositoryTeacher.saveAll(Arrays.asList(t1,t2,t3,t4));
 	}
 
 }
