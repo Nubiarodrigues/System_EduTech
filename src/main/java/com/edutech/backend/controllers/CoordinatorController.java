800cc980a4f9cb7b18471d556d4aacc2515d3f65
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-06-13 12:08:25 -0300

    feat(coordinator): criar camada de recursos com endpoints de update e delete

--- src/main/java/com/edutech/backend/controllers/CoordinatorController.java
+++ src/main/java/com/edutech/backend/controllers/CoordinatorController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +41,19 @@ public class CoordinatorController {
 		Coordinator newCoordinator = serviceCoordinator.createCoordinator(dto);
 		CoordinatorResponseDTO response = mapperCoordinator.toResponseDTO(newCoordinator);
 		return ResponseEntity.status(HttpStatus.CREATED).body(newCoordinator);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<CoordinatorResponseDTO> update(@PathVariable Long id,@RequestBody @Valid CoordinatorRequestDTO dto) {
+		Coordinator current = serviceCoordinator.updateCoordinator(id, dto);
+		CoordinatorResponseDTO response = mapperCoordinator.toResponseDTO(current);
+		return ResponseEntity.ok().body(response);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		serviceCoordinator.deleteCoordinator(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
