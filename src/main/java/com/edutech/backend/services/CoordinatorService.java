9f6922c693e7b67fddcc71dd2cf27163fd9b58d5
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-06-13 13:21:43 -0300

    refactor(coordinator): adiciona tratamento de exceções na camada service

--- src/main/java/com/edutech/backend/services/CoordinatorService.java
+++ src/main/java/com/edutech/backend/services/CoordinatorService.java
@@ -4,13 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.edutech.backend.dtos.CoordinatorRequestDTO;
 import com.edutech.backend.dtos.CoordinatorResponseDTO;
 import com.edutech.backend.entities.Coordinator;
+import com.edutech.backend.exceptions.ResourceNotFoundException;
 import com.edutech.backend.mapper.CoordinatorMapper;
 import com.edutech.backend.repositories.CoordinatorRepository;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -24,24 +27,32 @@ public class CoordinatorService {
 		return repositoryCoordinator.findAll().stream().map(CoordinatorResponseDTO::new).toList();
 	}
 
-	public Optional<Coordinator> findById(Long id) {
-		Optional<Coordinator> coordinator = repositoryCoordinator.findById(id);
-		return coordinator;
+	public Coordinator findById(Long id) {
+		return repositoryCoordinator.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
+	@Transactional
 	public Coordinator createCoordinator(CoordinatorRequestDTO dto) {
 		Coordinator newCoordinator = mapperCoordinator.toEntity(dto);
 		return repositoryCoordinator.save(newCoordinator);
 	}
 
+	@Transactional
 	public Coordinator updateCoordinator(Long id, CoordinatorRequestDTO dto) {
-		Coordinator entity = repositoryCoordinator.getReferenceById(id);
-		mapperCoordinator.updateStudentFromDTO(dto, entity);
-		return repositoryCoordinator.save(entity);
+		try {
+			Coordinator entity = repositoryCoordinator.getReferenceById(id);
+			mapperCoordinator.updateStudentFromDTO(dto, entity);
+			return repositoryCoordinator.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
+	@Transactional
 	public void deleteCoordinator(Long id) {
-		Optional<Coordinator> coordinator = repositoryCoordinator.findById(id);
+		Coordinator coordinator = repositoryCoordinator.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException(id));
 		repositoryCoordinator.deleteById(id);
 	}
 
