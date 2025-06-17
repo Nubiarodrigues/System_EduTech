2bc07c9f7cacf9cbb1f3379192697afa715c9fcd
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-06-17 16:08:40 -0300

    feat: criar classe utilitária responsável por gerar número de matrícula (registration) para os usuários cadastrados

---
+++ src/main/java/com/edutech/backend/utils/RegistrationGenerator.java
@@ -0,0 +1,22 @@
+package com.edutech.backend.utils;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class RegistrationGenerator {
+	
+	private Set<String> generatedRecords = new HashSet<>();
+	private Random random = new Random();
+
+	public String generateRegistrationUnique(String registration) {
+	    do {
+	        int number = random.nextInt(1_000_000);
+	        registration = String.format("%d60", number);
+	    } while (generatedRecords.contains(registration));
+	    
+	    generatedRecords.add(registration);
+	    return registration;
+	}
+
+}
