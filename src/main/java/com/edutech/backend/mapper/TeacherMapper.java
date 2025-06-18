26fc031abed7cd8e37cfaaf6b1ddf526a716bcc6
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-06-18 16:02:59 -0300

    feat(teacher): validar criação, tratar exceções e gerar matrícula

--- src/main/java/com/edutech/backend/mapper/TeacherMapper.java
+++ src/main/java/com/edutech/backend/mapper/TeacherMapper.java
@@ -11,17 +11,9 @@ import com.edutech.backend.entities.Teacher;
 @Mapper(componentModel = "spring")
 public interface TeacherMapper {
 
-	@Mapping(target = "email", ignore = true)
-	@Mapping(target = "id", ignore = true)
-	@Mapping(target = "password", ignore = true)
-	@Mapping(target = "role", ignore = true)
 	Teacher toEntity(TeacherRequestDTO dto);
 
 	TeacherResponseDTO toResponseDTO(Teacher entity);
-	
-	@Mapping(target = "email", ignore = true)
-	@Mapping(target = "id", ignore = true)
-	@Mapping(target = "password", ignore = true)
-	@Mapping(target = "role", ignore = true)
+
 	void updateTeacherFromDTO(TeacherRequestDTO dto, @MappingTarget Teacher entity);
 }
