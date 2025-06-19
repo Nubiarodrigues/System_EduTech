978302ae8205af39a9b8a6d7bb7f6234790d4908
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-06-19 13:56:27 -0300

    feat: criar serviço para consumir API externa com configuração de conexão e método privado para formatação

---
+++ src/main/java/com/edutech/backend/services/CepService.java
@@ -0,0 +1,43 @@
+package com.edutech.backend.services;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Service;
+
+import com.edutech.backend.dtos.AdressDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+@Service
+public class CepService {
+
+	private final OkHttpClient client = new OkHttpClient();
+
+	public String findAdress(String cep) {
+
+		Request request = new Request.Builder().url("https://viacep.com.br/ws/" + cep + "/json").build();
+
+		try (Response response = client.newCall(request).execute()) {
+
+			if (response.isSuccessful()) {
+				String json = response.body().string();
+				ObjectMapper mapper = new ObjectMapper();
+				AdressDTO dto = mapper.readValue(json, AdressDTO.class);
+				return formatAdress(dto);
+			} else {
+				throw new RuntimeException("Erro: " + response.code());
+			}
+
+		} catch (IOException e) {
+			throw new RuntimeException("Falha na requisição", e);
+		}
+	}
+
+	private String formatAdress(AdressDTO dto) {
+		return String.format("%s, %s - %s/%s - %s", dto.logradouro(), dto.bairro(), dto.localidade(), dto.uf(), dto.cep());
+	}
+
+}
