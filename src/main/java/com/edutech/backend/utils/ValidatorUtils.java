fba709aa6b91b5b9d7d05d6f0cdb940c37caab6e
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-06-25 12:18:11 -0300

    feat: criar método para validar cpf dentro da classe utilitária

--- src/main/java/com/edutech/backend/utils/ValidatorUtils.java
+++ src/main/java/com/edutech/backend/utils/ValidatorUtils.java
@@ -15,4 +15,68 @@ public class ValidatorUtils {
 		}
 	}
 
+	public static void validateCpf(String cpf) {
+
+		if (cpf == null || cpf.isEmpty()) {
+			throw new InvalidDataException("O CPF não deve ser nulo ou vazio");
+		}
+
+		if (!cpf.matches("^\\d{11}$")) {
+			throw new InvalidDataException("Formato do CPF é inválido");
+		}
+
+		int[] cpf_digitos = new int[cpf.length()];
+		boolean result = false;
+
+		for (int i = 0; i < 11; i++) {
+			cpf_digitos[i] = Character.getNumericValue(cpf.charAt(i));
+		}
+
+		result = checkCpf(cpf_digitos);
+
+		if (!result == true) {
+			throw new InvalidDataException("CPF inválido");
+		}
+
+	}
+
+	private static boolean checkCpf(int[] numberCpf) {
+		int i, j, sum, module, dv1, dv2;
+
+		// #1
+		sum = 0;
+		j = 10;
+
+		for (i = 0; i < 9; i++) {
+			sum += numberCpf[i] * j;
+			j -= 1;
+		}
+
+		module = sum % 11;
+
+		if (module < 2) {
+			dv1 = 0;
+		} else {
+			dv1 = 11 - module;
+		}
+
+		// #2
+		sum = 0;
+		j = 11;
+		for (i = 0; i < 10; i++) {
+			sum += numberCpf[i] * j;
+			j -= 1;
+		}
+
+		module = sum % 11;
+		if (module < 2) {
+			dv2 = 0;
+		} else {
+			dv2 = 11 - module;
+		}
+
+		return (numberCpf[9] == dv1) && (numberCpf[10] == dv2);
+
+	}
+
 }
