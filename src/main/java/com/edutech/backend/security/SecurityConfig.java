e70f8af31bd4f0f80beb43b7b4cbca9c6b37f88e
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-07-03 17:26:43 -0300

    feat(security): aplicar filtro de segurança para validar cada a requisição

--- src/main/java/com/edutech/backend/security/SecurityConfig.java
+++ src/main/java/com/edutech/backend/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.edutech.backend.security;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,10 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+
+    SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -30,7 +35,9 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/students/**").hasAnyRole("TEACHER", "COORDINATOR")
                         .requestMatchers(HttpMethod.GET, "/classrooms/**").hasRole("COORDINATOR")
                         .requestMatchers("*/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
                 )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
