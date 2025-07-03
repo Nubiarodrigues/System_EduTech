8c42678956fe701ba78cafb0bb24f7b2e9200008
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-07-03 11:22:23 -0300

    feat: implementar UserDetails na classe base e definir métodos de autenticação

--- src/main/java/com/edutech/backend/entities/User.java
+++ src/main/java/com/edutech/backend/entities/User.java
@@ -14,6 +14,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.management.relation.Role;
+import java.util.Collection;
+import java.util.List;
 
 @MappedSuperclass
 @Getter
@@ -22,7 +29,7 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @SuperBuilder
-public abstract class User {
+public abstract class User implements UserDetails {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,12 +44,60 @@ public abstract class User {
 	@Enumerated(EnumType.STRING)
 	protected RoleUser role;
 
-	public User(String name) {
-		this.name = name;
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		if (this.role == RoleUser.ADMIN) {
+			return List.of(
+					new SimpleGrantedAuthority("ROLE_ADMIN"),
+					new SimpleGrantedAuthority("ROLE_OPERATOR"),
+					new SimpleGrantedAuthority("ROLE_TEACHER"),
+					new SimpleGrantedAuthority("ROLE_STUDENT"),
+					new SimpleGrantedAuthority("ROLE_COORDINATOR")
+			);
+		} else if (this.role == RoleUser.OPERATOR) {
+			return List.of(
+					new SimpleGrantedAuthority("ROLE_OPERATOR"),
+					new SimpleGrantedAuthority("ROLE_STUDENT")
+			);
+		} else if (this.role == RoleUser.TEACHER) {
+			return List.of(
+					new SimpleGrantedAuthority("ROLE_TEACHER"),
+					new SimpleGrantedAuthority("ROLE_STUDENT")
+			);
+		} else if (this.role == RoleUser.COORDINATOR) {
+			return List.of(
+					new SimpleGrantedAuthority("ROLE_COORDINATOR"),
+					new SimpleGrantedAuthority("ROLE_STUDENT")
+			);
+		} else if (this.role == RoleUser.STUDENT){
+			return List.of(new SimpleGrantedAuthority("ROLE_STUDENT"));
+		}
+
+		return List.of();
 	}
 
-	public User(Long id) {
-		this.id = id;
+	@Override
+	public String getUsername() {
+		return email;
 	}
 
+	@Override
+	public boolean isAccountNonExpired() {
+		return UserDetails.super.isAccountNonExpired();
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return UserDetails.super.isAccountNonLocked();
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return UserDetails.super.isCredentialsNonExpired();
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return UserDetails.super.isEnabled();
+	}
 }
