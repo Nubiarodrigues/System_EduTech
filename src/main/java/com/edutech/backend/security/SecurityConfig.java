d5d009236845feb7db912f613304eabd83ef26e7
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-07-03 15:46:13 -0300

    chore(security): liberar acesso do H2

--- src/main/java/com/edutech/backend/security/SecurityConfig.java
+++ src/main/java/com/edutech/backend/security/SecurityConfig.java
@@ -17,11 +17,15 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .csrf(crsf -> crsf.disable())
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frame -> frame.disable()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/administrators").permitAll()
                         .requestMatchers("/students/**", "/classrooms/**").hasRole("OPERATOR")
                         .requestMatchers(HttpMethod.GET, "/students/**").hasAnyRole("TEACHER", "COORDINATOR")
                         .requestMatchers(HttpMethod.GET, "/classrooms/**").hasRole("COORDINATOR")
