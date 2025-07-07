9b7712d230eda6b241e8088b91e2161f9aebbe82
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-07-07 15:49:58 -0300

    feat(dto): ajustar dtos para trazer o professor ao consultar a disciplina

--- src/main/java/com/edutech/backend/dtos/DisciplineRequestDTO.java
+++ src/main/java/com/edutech/backend/dtos/DisciplineRequestDTO.java
@@ -2,5 +2,5 @@ package com.edutech.backend.dtos;
 
 import jakarta.validation.constraints.NotNull;
 
-public record DisciplineRequestDTO(@NotNull String name, @NotNull int workload, @NotNull String nameTeacher) {
+public record DisciplineRequestDTO(@NotNull String name, @NotNull int workload, @NotNull Long teacherId) {
 }
--- src/main/java/com/edutech/backend/dtos/DisciplineResponseDTO.java
+++ src/main/java/com/edutech/backend/dtos/DisciplineResponseDTO.java
@@ -1,13 +1,14 @@
 package com.edutech.backend.dtos;
 
 import com.edutech.backend.entities.Discipline;
+import com.edutech.backend.entities.Teacher;
 
-public record DisciplineResponseDTO(String name, int workload, String nameTeacher) {
+public record DisciplineResponseDTO(String name, int workload, Teacher teacher) {
 
     public DisciplineResponseDTO(Discipline discipline) {
         this (
                 discipline.getName(),
                 discipline.getWorkload(),
-                discipline.getTeacher() != null ? discipline.getTeacher().getName() : null);
+                discipline.getTeacher());
     }
 }
