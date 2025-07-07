171c33351d5836265e9c537adcf10ee7d2f5ccfb
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-07-07 15:55:28 -0300

    feat(service/controller): criar controle com endpoints de CRUD e no service, criar o método de findById

--- src/main/java/com/edutech/backend/services/DisciplineService.java
+++ src/main/java/com/edutech/backend/services/DisciplineService.java
@@ -3,9 +3,11 @@ package com.edutech.backend.services;
 import com.edutech.backend.dtos.DisciplineRequestDTO;
 import com.edutech.backend.dtos.DisciplineResponseDTO;
 import com.edutech.backend.entities.Discipline;
+import com.edutech.backend.entities.Teacher;
 import com.edutech.backend.exceptions.ResourceNotFoundException;
 import com.edutech.backend.mapper.DisciplineMapper;
 import com.edutech.backend.repositories.DisciplineRepository;
+import com.edutech.backend.repositories.TeacherRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,30 +20,40 @@ public class DisciplineService {
 
     private final DisciplineRepository repositoryDiscipline;
     private final DisciplineMapper mapperDiscipline;
+    private final TeacherRepository repositoryTeacher;
 
-    public List<DisciplineResponseDTO> findAll(){
+    public List<DisciplineResponseDTO> findAll() {
         return repositoryDiscipline.findAll().stream().map(DisciplineResponseDTO::new).toList();
     }
 
-    public Discipline createDiscipline(DisciplineRequestDTO dto){
-        Discipline newDiscipline = mapperDiscipline.toEntity(dto);
-        return  repositoryDiscipline.save(newDiscipline);
+    public Discipline findById(Long id) {
+        return repositoryDiscipline.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
-    public Discipline updateDiscipline(Long id, DisciplineRequestDTO dto){
+    public Discipline createDiscipline(DisciplineRequestDTO dto) {
+        Discipline newDiscipline = mapperDiscipline.toEntity(dto);
+
+        Teacher teacher = repositoryTeacher.findById(dto.teacherId())
+                .orElseThrow(() -> new ResourceNotFoundException("Professor não encontrado!"));
+
+        newDiscipline.setTeacher(teacher);
+        return repositoryDiscipline.save(newDiscipline);
+    }
+
+    public Discipline updateDiscipline(Long id, DisciplineRequestDTO dto) {
         try {
             Discipline entity = repositoryDiscipline.getReferenceById(id);
-            mapperDiscipline.updateDisciplineFromDTO(dto, entity);;
+            mapperDiscipline.updateDisciplineFromDTO(dto, entity);
+            ;
             return repositoryDiscipline.save(entity);
         } catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException(id);
         }
     }
 
-    public void deleteDiscipline(Long id){
+    public void deleteDiscipline(Long id) {
         Discipline discipline = repositoryDiscipline.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException(id));
         repositoryDiscipline.delete(discipline);
     }
-
 }
