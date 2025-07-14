e1358d58cb0c9b55a2d0a9de76601a2a90bc50d4
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-07-14 13:13:03 -0300

    feat: adicionar novo atributo que será a carga horária atribuída a um professor e adptar dto

--- src/main/java/com/edutech/backend/dtos/TeacherResponseDTO.java
+++ src/main/java/com/edutech/backend/dtos/TeacherResponseDTO.java
@@ -1,19 +1,11 @@
 package com.edutech.backend.dtos;
 
-import com.edutech.backend.entities.Teacher;
 import com.edutech.backend.enuns.RoleUser;
 
 public record TeacherResponseDTO(
 		Long id,
 		String name,
 		String email,
-		RoleUser role) {
-
-	public TeacherResponseDTO(Teacher teacher) {
-		this(
-				teacher.getId(),
-				teacher.getName(),
-				teacher.getEmail(),
-				teacher.getRole());
-	}
+		RoleUser role,
+		Integer workloadAllocated) {
 }
