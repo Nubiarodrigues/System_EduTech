145fe3b55941f37a7be9073c77af2e3759f94b31
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-07-14 09:45:25 -0300

    feat(dto): adaptar DTOs para exposição adequada no retorno do JSON

--- src/main/java/com/edutech/backend/dtos/DisciplineRequestDTO.java
+++ src/main/java/com/edutech/backend/dtos/DisciplineRequestDTO.java
@@ -2,5 +2,5 @@ package com.edutech.backend.dtos;
 
 import jakarta.validation.constraints.NotNull;
 
-public record DisciplineRequestDTO(@NotNull String name, @NotNull int workload, @NotNull Long teacherId) {
+public record DisciplineRequestDTO(@NotNull String name, @NotNull int workload) {
 }
--- src/main/java/com/edutech/backend/dtos/TeacherResponseDTO.java
+++ src/main/java/com/edutech/backend/dtos/TeacherResponseDTO.java
@@ -3,27 +3,17 @@ package com.edutech.backend.dtos;
 import com.edutech.backend.entities.Teacher;
 import com.edutech.backend.enuns.RoleUser;
 
-import java.time.LocalDate;
-
 public record TeacherResponseDTO(
 		Long id,
 		String name,
 		String email,
-		RoleUser role,
-		LocalDate dateBirth,
-		Integer workloadTotal,
-		String registration,
-		String address) {
+		RoleUser role) {
 
 	public TeacherResponseDTO(Teacher teacher) {
 		this(
 				teacher.getId(),
 				teacher.getName(),
 				teacher.getEmail(),
-				teacher.getRole(),
-				teacher.getDateBirth(),
-				teacher.getWorkloadTotal(),
-				teacher.getRegistration(),
-				teacher.getAddress());
+				teacher.getRole());
 	}
 }
