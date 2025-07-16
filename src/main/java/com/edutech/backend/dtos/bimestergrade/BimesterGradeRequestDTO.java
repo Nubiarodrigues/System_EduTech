ba5eac8d05556b32b27b9627445020cafe9f822f
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-07-16 12:57:19 -0300

    refactor(dto): melhorar legibilidade

--- src/main/java/com/edutech/backend/dtos/bimestergrade/BimesterGradeRequestDTO.java
+++ src/main/java/com/edutech/backend/dtos/bimestergrade/BimesterGradeRequestDTO.java
@@ -3,7 +3,6 @@ package com.edutech.backend.dtos.bimestergrade;
 import jakarta.validation.constraints.NotNull;
 
 public record BimesterGradeRequestDTO(
-        @NotNull Long idStudent,
         @NotNull Integer bimester,
         @NotNull Double grade1,
         @NotNull Double grade2,
