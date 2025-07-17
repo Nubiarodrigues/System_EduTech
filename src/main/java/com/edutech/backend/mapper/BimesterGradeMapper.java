2904c7d52be35ab2cd2df769f0736e31b4f0bfc7
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-07-17 12:42:28 -0300

    fix(mapper): corrigir mapeamento para usar construtor personalizado do DTO

--- src/main/java/com/edutech/backend/mapper/BimesterGradeMapper.java
+++ src/main/java/com/edutech/backend/mapper/BimesterGradeMapper.java
@@ -1,9 +1,12 @@
 package com.edutech.backend.mapper;
 
+import com.edutech.backend.dtos.bimestergrade.BimesterGradeFinalRequestDTO;
+import com.edutech.backend.dtos.bimestergrade.BimesterGradeFinalResponseDTO;
 import com.edutech.backend.dtos.bimestergrade.BimesterGradeRequestDTO;
 import com.edutech.backend.dtos.bimestergrade.BimesterGradeResponseDTO;
 import com.edutech.backend.entities.BimesterGrade;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
@@ -11,8 +14,27 @@ public interface BimesterGradeMapper {
 
     BimesterGrade toEntity(BimesterGradeRequestDTO dto);
 
+    @Mapping(target = "nameStudent", source = "student.name")
+    @Mapping(target = "nameDiscipline", source = "discipline.name")
+    @Mapping(target = "situation", source = "situation")
+    @Mapping(target = "bimester", source = "bimester")
+    @Mapping(target = "finalAverage", expression = "java(entity.finalAverageCalculation())")
     BimesterGradeResponseDTO toResponseDTO(BimesterGrade entity);
 
+    @Mapping(target = "nameStudent", source = "student.name")
+    @Mapping(target = "nameDiscipline", source = "discipline.name")
+    @Mapping(target = "situation", source = "situation")
+    @Mapping(target = "bimester", source = "bimester")
+    @Mapping(target = "averageGlobal", expression = "java(entity.finalGlobalCalculation())")
+    BimesterGradeFinalResponseDTO toResponseFinalDTO(BimesterGrade entity);
+
+    @Mapping(target = "bimester", source = "bimester")
+    @Mapping(target = "grade1", source = "grade1")
+    @Mapping(target = "grade2", source = "grade2")
+    @Mapping(target = "grade3", source = "grade3")
+    @Mapping(target = "grade4", source = "grade4")
     void updateBimesterFromDTO(BimesterGradeRequestDTO dto, @MappingTarget BimesterGrade entity);
 
+    @Mapping(target = "gradeFinal", source = "gradeFinal")
+    void updateBimesterFinalFromDTO(BimesterGradeFinalRequestDTO dto, @MappingTarget BimesterGrade entity);
 }
