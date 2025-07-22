701f898148ebbcafca276871658ec21b77986bf1
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-07-22 16:29:52 -0300

    feat(service): implementar busca de ocorrências por aluno conforme enum e validar limite em cada nova criação

--- src/main/java/com/edutech/backend/services/DisciplinaryRecordService.java
+++ src/main/java/com/edutech/backend/services/DisciplinaryRecordService.java
@@ -10,6 +10,7 @@ import com.edutech.backend.exceptions.ResourceNotFoundException;
 import com.edutech.backend.mapper.DisciplinaryRecordMapper;
 import com.edutech.backend.repositories.DisciplinaryRecordRepository;
 import com.edutech.backend.repositories.StudentRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -32,46 +33,85 @@ public class DisciplinaryRecordService {
     }
 
     @Transactional
-    public DisciplinaryRecord create(DisciplinaryRecordRequestDTO dto, String responsible){
+    public DisciplinaryRecord create(DisciplinaryRecordRequestDTO dto, Long idStudent, String responsible){
         DisciplinaryRecord newDisciplinaryRecord = mapperDisciplinaryRecord.toEntity(dto);
 
-        Student student = repositoryStudent.findById(dto.idStudent())
+        Student student = repositoryStudent.findById(idStudent)
                 .orElseThrow(() -> new ResourceNotFoundException("O aluno não existe."));
 
         newDisciplinaryRecord.setStudent(student);
         newDisciplinaryRecord.setResponsible(responsible);
 
-        verifyLimitWarningsAndSuspensions(newDisciplinaryRecord);
+        verifyLimitWarningsAndSuspensions(newDisciplinaryRecord, idStudent);
         repositoryDisciplinaryRecord.save(newDisciplinaryRecord);
         return newDisciplinaryRecord;
     }
 
-    private void verifyLimitWarningsAndSuspensions(DisciplinaryRecord entity){
+    private void verifyLimitWarningsAndSuspensions(DisciplinaryRecord entity, Long idStudent) {
 
-        if(entity.getVerbalWarnings() < 2){
-            if(entity.getOccurrence().equals(TypeOccurrence.LEVE)){
-                entity.setVerbalWarnings(entity.getVerbalWarnings() + 1);
+        List<DisciplinaryRecord> previousRecords = repositoryDisciplinaryRecord.findByStudentId(idStudent);
+
+        int warningsVerbal = previousRecords.stream()
+                .filter(r -> r.getOccurrence() == TypeOccurrence.LEVE)
+                .mapToInt(DisciplinaryRecord::getVerbalWarnings).sum();
+
+        int warningsFormal = previousRecords.stream()
+                .filter(r -> r.getOccurrence() == TypeOccurrence.ADVERTENCIA)
+                .mapToInt(DisciplinaryRecord::getWarningsIssued).sum();
+
+        int warningsSuspension = previousRecords.stream()
+                .filter(r -> r.getOccurrence() == TypeOccurrence.SUSPENSAO)
+                .mapToInt(DisciplinaryRecord::getSuspensionsIssued).sum();
+
+        if (entity.getOccurrence().equals(TypeOccurrence.LEVE)) {
+            if ((warningsVerbal + 1) > 2) {
+                throw new InvalidDataException("O limite de advertências verbais foi excedido.");
             }
-        } else {
-            throw new InvalidDataException("O limite de advertências verbais foi excedido.");
+            entity.setVerbalWarnings(1);
         }
 
-        if(entity.getWarningsIssued() < 3){
-            if(entity.getOccurrence().equals(TypeOccurrence.ADVERTENCIA)){
-                entity.setWarningsIssued(entity.getWarningsIssued() + 1);
+        if (entity.getOccurrence().equals(TypeOccurrence.ADVERTENCIA)) {
+            if ((warningsFormal + 1) > 3) {
+                throw new InvalidDataException("O limite de advertências formais foi excedido. É necessário buscar ajuda do Conselho Tutelar.");
             }
-        } else {
-            throw new InvalidDataException("O limite de advertências formais foi excedido. É necessário buscar ajuda do Conselho Tutelar.");
+            entity.setWarningsIssued(1);
         }
 
-        if(entity.getSuspensionsIssued() < 3){
-            if(entity.getOccurrence().equals(TypeOccurrence.SUSPENSAO)){
-                entity.setSuspensionsIssued(entity.getSuspensionsIssued() + 1);
+        if (entity.getOccurrence().equals(TypeOccurrence.SUSPENSAO)) {
+            if ((warningsSuspension + 1) > 3) {
+                entity.setExpulsion(true);
+                throw new InvalidDataException("O limite de suspensões formais foi excedido. Nesta etapa, não há mais o que fazer.");
             }
-        } else {
-            throw new InvalidDataException("O limite de suspensões formais foi excedido. Nesta etapa, não há mais o que fazer.");
+            entity.setSuspensionsIssued(1);
+        }
+    }
+
+    @Transactional
+    public DisciplinaryRecord update(Long id, Long idStudent, DisciplinaryRecordRequestDTO dto, String responsible){
+        try {
+            DisciplinaryRecord current = repositoryDisciplinaryRecord.findById(id).orElseThrow(() -> new ResourceNotFoundException("Ocorrência não existe. "));
+            mapperDisciplinaryRecord.updateDisciplinaryRecordFromDTO(dto, current);
+
+            if(!current.getResponsible().equals(responsible)){
+               throw new InvalidDataException("Você não tem permissão para atualizar este recurso.");
+            }
+
+            repositoryDisciplinaryRecord.save(current);
+            return current;
+        } catch(EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    @Transactional
+    public void delete(Long id, String responsible){
+        DisciplinaryRecord disciplinaryRecord = repositoryDisciplinaryRecord.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Ocorrência não existe. "));
+
+        if(!disciplinaryRecord.getResponsible().equals(responsible)){
+            throw new InvalidDataException("Você não tem permissão para atualizar este recurso.");
         }
 
-        entity.setExpulsion(true);
+        repositoryDisciplinaryRecord.delete(disciplinaryRecord);
     }
 }
