3fbe4974b100c7e3cdea5c2e8ebd9a9167ebdd29
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-07-22 16:27:28 -0300

    feat(repository): criar método para buscar ocorrências por aluno

--- src/main/java/com/edutech/backend/repositories/DisciplinaryRecordRepository.java
+++ src/main/java/com/edutech/backend/repositories/DisciplinaryRecordRepository.java
@@ -3,5 +3,10 @@ package com.edutech.backend.repositories;
 import com.edutech.backend.entities.DisciplinaryRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DisciplinaryRecordRepository extends JpaRepository<DisciplinaryRecord,Long> {
+
+    List<DisciplinaryRecord> findByStudentId(Long id);
+
 }
