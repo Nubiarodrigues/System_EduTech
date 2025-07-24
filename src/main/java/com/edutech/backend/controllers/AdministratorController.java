b6e2223e1f76d9087ef02c5c6eac5332039ab44d
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-07-24 16:29:32 -0300

    refactor: melhorar legibilidade dos métodos do service da entidade Administrator, refletir alterações no controller e atualizar imports

--- src/main/java/com/edutech/backend/controllers/AdministratorController.java
+++ src/main/java/com/edutech/backend/controllers/AdministratorController.java
@@ -1,27 +1,18 @@
 package com.edutech.backend.controllers;
 
-import java.net.URI;
-import java.util.List;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import com.edutech.backend.dtos.administrator.AdministratorRequestDTO;
 import com.edutech.backend.dtos.administrator.AdministratorResponseDTO;
 import com.edutech.backend.entities.Administrator;
 import com.edutech.backend.mapper.AdministratorMapper;
 import com.edutech.backend.services.AdministratorService;
-
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/administrators")
@@ -39,7 +30,7 @@ public class AdministratorController {
 
 	@PostMapping
 	public ResponseEntity<AdministratorResponseDTO> create(@RequestBody @Valid AdministratorRequestDTO dto) {
-		Administrator newAdministrator = serviceAdministrator.createAdmin(dto);
+		Administrator newAdministrator = serviceAdministrator.create(dto);
 		URI location = ServletUriComponentsBuilder
 				.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(newAdministrator.getId())
@@ -51,14 +42,14 @@ public class AdministratorController {
 	@PutMapping("/{id}")
 	public ResponseEntity<AdministratorResponseDTO> update(@PathVariable Long id,
 			@RequestBody @Valid AdministratorRequestDTO dto) {
-		Administrator current = serviceAdministrator.updateAdmin(id, dto);
+		Administrator current = serviceAdministrator.update(id, dto);
 		AdministratorResponseDTO response = mapperAdministrator.toResponseDTO(current);
 		return ResponseEntity.ok(response);
 	}
 
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
-		serviceAdministrator.deleteAdmin(id);
+		serviceAdministrator.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/edutech/backend/services/AdministratorService.java
+++ src/main/java/com/edutech/backend/services/AdministratorService.java
@@ -1,11 +1,5 @@
 package com.edutech.backend.services;
 
-import java.util.List;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.edutech.backend.dtos.administrator.AdministratorRequestDTO;
 import com.edutech.backend.dtos.administrator.AdministratorResponseDTO;
 import com.edutech.backend.entities.Administrator;
@@ -15,9 +9,13 @@ import com.edutech.backend.mapper.AdministratorMapper;
 import com.edutech.backend.repositories.AdministratorRepository;
 import com.edutech.backend.utils.RegistrationGenerator;
 import com.edutech.backend.utils.ValidatorUtils;
-
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -31,27 +29,29 @@ public class AdministratorService {
 	}
 
 	@Transactional
-	public Administrator createAdmin(AdministratorRequestDTO dto) {
+	public Administrator create(AdministratorRequestDTO dto) {
 		Administrator newAdministrator = mapperAdministrator.toEntity(dto);
 		prepareCreateAdministrator(newAdministrator, dto);
 		return repositoryAdministrator.save(newAdministrator);
 	}
 
 	@Transactional
-	public Administrator updateAdmin(Long id, AdministratorRequestDTO dto) {
+	public Administrator update(Long id, AdministratorRequestDTO dto) {
 		try {
-			Administrator entity = repositoryAdministrator.getReferenceById(id);
-			mapperAdministrator.updateAdministratorFromDTO(dto, entity);
-			return repositoryAdministrator.save(entity);
+			Administrator current = repositoryAdministrator
+					.findById(id).orElseThrow(() -> new ResourceNotFoundException("Administrator com ID: " + id + " não existe."));
+
+			mapperAdministrator.updateAdministratorFromDTO(dto, current);
+			return repositoryAdministrator.save(current);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException(id);
 		}
 	}
 
 	@Transactional
-	public void deleteAdmin(Long id) {
+	public void delete(Long id) {
 		Administrator administrator = repositoryAdministrator.findById(id)
-				.orElseThrow(() -> new ResourceNotFoundException(id));
+				.orElseThrow(() -> new ResourceNotFoundException("Administrator com ID: " + id + " não existe."));
 		repositoryAdministrator.delete(administrator);
 	}
 	
@@ -71,5 +71,4 @@ public class AdministratorService {
 				.generateRegistrationUnique(admin.getRegistration()));
 		
 	}
-
 }
