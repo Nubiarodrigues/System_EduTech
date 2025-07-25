7f93677ea89085c5bdb3d63f1510c234eb43a247
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-07-25 14:08:41 -0300

    refactor(dto): adicionar campo idSchool no DTO de requisição e response para lidar com criação do primeiro usuário

--- src/main/java/com/edutech/backend/dtos/administrator/AdministratorRequestDTO.java
+++ src/main/java/com/edutech/backend/dtos/administrator/AdministratorRequestDTO.java
@@ -1,13 +1,13 @@
 package com.edutech.backend.dtos.administrator;
 
 import com.edutech.backend.enuns.RoleUser;
-
 import jakarta.validation.constraints.NotNull;
 
 public record AdministratorRequestDTO(
 		@NotNull String name,
 		@NotNull String email,
 		@NotNull RoleUser role,
-		@NotNull String password) {
+		@NotNull String password,
+		@NotNull Long idSchool) {
 
 }
--- src/main/java/com/edutech/backend/dtos/administrator/AdministratorResponseDTO.java
+++ src/main/java/com/edutech/backend/dtos/administrator/AdministratorResponseDTO.java
@@ -3,9 +3,9 @@ package com.edutech.backend.dtos.administrator;
 import com.edutech.backend.entities.Administrator;
 import com.edutech.backend.enuns.RoleUser;
 
-public record AdministratorResponseDTO(Long id, String name, String email, RoleUser role, String registration) {
+public record AdministratorResponseDTO(Long id, String name, String email, RoleUser role, String registration, Long idSchool) {
 
 	public AdministratorResponseDTO(Administrator admin) {
-		this(admin.getId(), admin.getName(), admin.getEmail(), admin.getRole(), admin.getRegistration());
+		this(admin.getId(), admin.getName(), admin.getEmail(), admin.getRole(), admin.getRegistration(), admin.getIdSchool());
 	}
 }
