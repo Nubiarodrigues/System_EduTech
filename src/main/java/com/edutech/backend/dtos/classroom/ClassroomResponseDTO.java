36900dba32477400093772ee038668fb27169c46
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-07-25 15:41:29 -0300

    refactor: adicionar os DTOs no DTO de resposta da turma e fazer o Mapper reconhecer as alterações

--- src/main/java/com/edutech/backend/dtos/classroom/ClassroomResponseDTO.java
+++ src/main/java/com/edutech/backend/dtos/classroom/ClassroomResponseDTO.java
@@ -1,9 +1,9 @@
 package com.edutech.backend.dtos.classroom;
 
+import com.edutech.backend.dtos.discipline.DisciplineSimpleResponseDTO;
 import com.edutech.backend.dtos.schoolnotice.SchoolNoticesResponseDTO;
+import com.edutech.backend.dtos.student.StudentSimpleResponseDTO;
 import com.edutech.backend.entities.Classroom;
-import com.edutech.backend.entities.Discipline;
-import com.edutech.backend.entities.Student;
 
 import java.util.Collections;
 import java.util.List;
@@ -13,8 +13,8 @@ public record ClassroomResponseDTO(
 		Long id,
 		Integer series,
 		String identifierSeries,
-		List<Discipline> disciplines,
-		List<Student> students,
+		List<DisciplineSimpleResponseDTO> disciplines,
+		List<StudentSimpleResponseDTO> students,
 		String coordinatorName,
 		List<SchoolNoticesResponseDTO>  schoolNotices) {
 
@@ -23,8 +23,8 @@ public record ClassroomResponseDTO(
 				classroom.getId(),
 				classroom.getSeries(),
 				classroom.getIdentifierSeries(),
-				classroom.getDisciplines(),
-				classroom.getStudents(),
+				classroom.getDisciplines() != null ? classroom.getDisciplines().stream().map(DisciplineSimpleResponseDTO::new).collect(Collectors.toList()) : Collections.emptyList(),
+				classroom.getStudents() != null ? classroom.getStudents().stream().map(StudentSimpleResponseDTO::new).collect(Collectors.toList()) : Collections.emptyList(),
 				classroom.getCoordinatorClass() != null ? classroom.getCoordinatorClass().getName() : null,
 				classroom.getSchoolNotices() != null ? classroom.getSchoolNotices().stream().map(SchoolNoticesResponseDTO::new).collect(Collectors.toList()) : Collections.emptyList());
 	}
--- src/main/java/com/edutech/backend/mapper/ClassroomMapper.java
+++ src/main/java/com/edutech/backend/mapper/ClassroomMapper.java
@@ -15,8 +15,9 @@ public interface ClassroomMapper {
 
 	Classroom toEntity(ClassroomRequestDTO dto);
 
-	@Mapping(target = "disciplines", source = "entity.disciplines")
+	@Mapping(target = "disciplines", expression = "java(entity.getDisciplines() != null ? entity.getDisciplines().stream().map(DisciplineSimpleResponseDTO::new).collect(Collectors.toList()) : Collections.emptyList())")
 	@Mapping(target = "coordinatorName", expression = "java(entity.getCoordinatorClass() != null ? entity.getCoordinatorClass().getName() : null)")
+	@Mapping(target = "students", expression = "java(entity.getStudents() != null ? entity.getStudents().stream().map(StudentSimpleResponseDTO::new).collect(Collectors.toList()) : Collections.emptyList())")
 	@Mapping(target = "schoolNotices", expression = "java(entity.getSchoolNotices() != null ? entity.getSchoolNotices().stream().map(SchoolNoticesResponseDTO::new).collect(Collectors.toList()) : Collections.emptyList())")
 	ClassroomResponseDTO toResponseDTO(Classroom entity);
 
