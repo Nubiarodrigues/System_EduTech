1cc1465a576f1b41cb4174782b365190faf12412
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-07-31 10:45:49 -0300

    feat(page): criar página Home com conteúdo textual inicial

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import SideBar from '../../components/SideBar/SideBar'
+import styles from './Home.module.css'
+
+const Home = () => {
+  return (
+    <div className={styles.home_container}>
+        <p>Estatísticas</p>
+        <p>Estatísticas</p>
+        <p>Estatísticas</p>
+    </div>
+  )
+}
+
+export default Home
