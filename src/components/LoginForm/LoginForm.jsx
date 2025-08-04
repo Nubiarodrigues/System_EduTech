dfca7a979cbc56bce4b9388e4a93912eeaf62701
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-08-04 15:39:13 -0300

    feat(page/components): criar componente para form de login e página para a tela de login (provisória)

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useLoginActions } from '../../hooks/useLoginActions';
+import styles from './LoginForm.module.css';
+import logo_login from '/src/assets/image/logo_login.png';
+
+const LoginForm = () => {
+
+    const navigate = useNavigate();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const { sendCredentials, loading, error } = useLoginActions();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const result = await sendCredentials({ email, password });
+
+            if (result) {
+                navigate("/");
+            }
+
+        } catch {
+            alert("Erro ao logar.");
+        }
+    }
+
+    return (
+        <div className={styles.container_global}>
+
+            <div>
+                <img className={styles.logo} src={logo_login} alt="logo-login" />
+                <hr />
+
+                <p>Faça seu login para continuar</p>
+            </div>
+
+            <form onSubmit={handleSubmit}>
+
+                <label>
+                    Email
+                    <input
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        required />
+                </label>
+
+                <label>
+                    Senha
+                    <input
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        required />
+                </label>
+
+                <p className={styles.parag}>Esqueceu a senha? </p>
+
+                <button type='submit' disabled={loading}>
+                    {loading ? "Logando..." : "Entrar"}
+                </button>
+                {error && <p>Erro: {error.message}</p>}
+
+            </form>
+
+        </div>
+    )
+}
+
+export default LoginForm
