8db3c1d9e158c0d97fb00dac57a1d008e7c2dfab
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-08-06 16:01:15 -0300

    feat(dto): criar DTO  para testar se traz o usuário logado através do set-cookie

---
+++ src/main/java/com/edutech/backend/dtos/authenticate/UserResponseDTO.java
@@ -0,0 +1,16 @@
+package com.edutech.backend.dtos.authenticate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponseDTO {
+
+    private String name;
+    private String role;
+}
