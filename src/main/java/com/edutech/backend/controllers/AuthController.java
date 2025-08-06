0ffe015b440cc0fe4c690c7835677cbe086c034a
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-08-06 15:55:39 -0300

    feat(controller): criar configuração do HttpOnly, endpoint de logout e de buscar usuário autenticado

--- src/main/java/com/edutech/backend/controllers/AuthController.java
+++ src/main/java/com/edutech/backend/controllers/AuthController.java
@@ -1,18 +1,23 @@
 package com.edutech.backend.controllers;
 
 import com.edutech.backend.dtos.authenticate.AuthenticationRequestDTO;
-import com.edutech.backend.dtos.authenticate.LoginResponseDTO;
+import com.edutech.backend.dtos.authenticate.UserResponseDTO;
 import com.edutech.backend.entities.User;
 import com.edutech.backend.security.TokenService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -23,12 +28,50 @@ public class AuthController {
     AuthenticationManager authenticationManager;
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody @Valid AuthenticationRequestDTO dto){
+    public ResponseEntity<?> login(@RequestBody @Valid AuthenticationRequestDTO dto, HttpServletResponse response){
         var emailPassword = new UsernamePasswordAuthenticationToken(dto.email(), dto.password());
         var auth = this.authenticationManager.authenticate(emailPassword);
 
         var token = tokenService.generateToken((User) auth.getPrincipal());
 
-        return ResponseEntity.ok(new LoginResponseDTO(token));
+        ResponseCookie cookie = ResponseCookie.from("access_token", token)
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .maxAge(30 *  60)
+                .sameSite("Lax")
+                .build();
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+        return ResponseEntity.ok(Map.of("message", "Login realizado com sucesso!"));
+    }
+
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletResponse response) {
+        ResponseCookie cookie = ResponseCookie.from("access_token", "")
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .maxAge(0)
+                .sameSite("Lax")
+                .build();
+
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+
+        return ResponseEntity.ok("Logout realizado com sucesso!");
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<UserResponseDTO> getAuthenticatedUser(@AuthenticationPrincipal User user) {
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        String role = user.getAuthorities().stream()
+                .findFirst()
+                .map(a -> a.getAuthority())
+                .orElse("");
+
+        UserResponseDTO response = new UserResponseDTO(user.getUsername(), role);
+        return ResponseEntity.ok(response);
     }
 }
