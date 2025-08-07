805e7d5bee697c56428d4f95b6420759b1e821c5
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-08-07 14:34:21 -0300

    chore: habilitar suporte a HTTPS

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync('D:/system_edutech/cert_mkcert/localhost-key.pem'),
+      cert: fs.readFileSync('D:/system_edutech/cert_mkcert/localhost.pem'),
+    },
+    port: 5173
+  }
 })
