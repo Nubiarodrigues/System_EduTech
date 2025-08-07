26883c8be29762fb103e7d87378746ee8f3e402d
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-08-07 14:51:36 -0300

    feat(components): implementar função de logout no SideBar e vincular ao botão de sair

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,19 +1,37 @@
-import { Link } from 'react-router-dom'
-import logo from '/src/assets/image/logo.png'
-import styles from './SideBar.module.css'
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuthContext } from '../../contexts/auth/AuthContext';
+import styles from './SideBar.module.css';
+import logo from '/src/assets/image/logo.png';
 
-export default function SideBar(){
+export default function SideBar() {
+
+    const { user, isLoadingLoggedUser, login, logout } = useAuthContext();
+
+    const navigate = useNavigate();
+
+    const handleLogout = async (e) => {
+        const sucess = await logout();
+
+        if (sucess) {
+            navigate("/login");
+        }
+    };
+
     return (
         <div className={styles.sidebar_container}>
             <div>
                 <img className={styles.logo} src={logo} alt="logo" />
-                <hr/>
+                <hr />
             </div>
             <nav>
                 <Link className={styles.item} to="/">Home</Link>
                 <Link className={styles.item} to="/">Turmas</Link>
                 <Link className={styles.item} to="/">Professores</Link>
-                <Link className={styles.item} to="/">Sair</Link>
+                <Link className={styles.item} to="/school-details">Escola</Link>
+
+                <button type='button' onClick={handleLogout}>
+                    Sair
+                </button>
             </nav>
         </div>
     )
