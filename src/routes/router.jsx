d07b9ab2f9bd6d8f4c66cfedd5858e7c607c11dd
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-08-07 14:42:42 -0300

    feat(router): criar rotas aninhadas para desenvolvimento isolado de páginas

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -4,6 +4,7 @@ import MainLayout from "../layouts/MainLayout";
 import Home from '../pages/Home/Home';
 import SchoolPanel from "../pages/School/SchoolPanel";
 import LoginScreen from '../pages/LoginScreen/Login';
+import AdminForm from "../components/AdminForm/AdminForm";
 
 export const router = createBrowserRouter([
     {
@@ -12,12 +13,21 @@ export const router = createBrowserRouter([
         children: [
             { index: true, element: <Home /> },
             { path: "/school-details", element: <SchoolPanel /> },
-            { path: "/register-school", element: <FormRegister /> },
         ]
     },
 
     {
         path: "/login",
         element: <LoginScreen />
+    },
+
+    {
+        path: "/createAdmin",
+        element: <AdminForm />
+    },
+
+    {
+        path: "/createSchool",
+        element: <FormRegister />
     }
 ])
