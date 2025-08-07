3e70a215dd30944b0a211909a4496c7208314efa
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-08-07 15:12:49 -0300

    feat(config): incluir anotação no CorsRegistry para não receber o registry nulo

--- src/main/java/com/edutech/backend/config/CorsConfig.java
+++ src/main/java/com/edutech/backend/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.edutech.backend.config;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,7 +12,7 @@ public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NotNull CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("https://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
