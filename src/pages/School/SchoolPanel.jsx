1dd995f5762891fc3db5dd72cbbdaf79e3120796
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-08-07 14:58:40 -0300

    feat(page): criar página para representar a escola (detalhes/edição)

---
+++ src/pages/School/SchoolPanel.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from '../../contexts/auth/AuthContext';
+import useSchoolFindById from "../../hooks/schoolActions/useSchoolFindById";
+import styles from './SchoolPanel.module.css';
+
+const SchoolPanel = () => {
+
+  const navigate = useNavigate();
+  const { findById, error } = useSchoolFindById();
+  const { user } = useAuthContext();
+  const [school, setSchool] = useState(null);
+
+  const handleFindBySchool = async () => {
+    console.log("Buscando escola com ID:", user?.idSchool);
+
+    try {
+      const schoolData = await findById(user.idSchool);
+      setSchool(schoolData);
+    } catch (error) {
+      console.error("Erro ao buscar escola:", error);
+    }
+  };
+
+
+  useEffect(() => {
+    if (user?.idSchool) {
+      handleFindBySchool();
+    } else {
+      console.log("Usuário ou idSchool indefinido");
+    }
+  }, [user]);
+
+  return (
+    <div className={styles.container_school}>
+
+      <div className={styles.container_details}>
+        <p>Nome da instituição: {school?.name || "Carregando..."}</p>
+        <p>Endereço: {school?.address || "Carregando..."}</p>
+        <p>Capacidade: </p>
+        <p>Modalidade: </p>
+        <p>Oferta: {school?.stages || "Carregando..."}</p>
+      </div>
+
+      <button onClick={() => navigate('/register-school')}>
+        Cadastrar nova escola
+      </button>
+
+    </div>
+  )
+}
+
+export default SchoolPanel;
