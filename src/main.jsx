b608571b7d294e21b1009fa2848b4976e7268c95
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-08-07 14:39:49 -0300

    chore(main): envolver aplicação com AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import AuthProvider from './contexts/auth/AuthProvider'
 import './index.css'
 import { router } from './routes/router'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+  <React.StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
+);
