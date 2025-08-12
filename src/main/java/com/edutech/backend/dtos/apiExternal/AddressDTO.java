3de5d03aa65075d8119f5b6ba2e7731a78637b29
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-08-12 10:27:20 -0300

    refactor(dto): aplicar anotações do Validation para o DTO de CEP

--- src/main/java/com/edutech/backend/dtos/apiExternal/AddressDTO.java
+++ src/main/java/com/edutech/backend/dtos/apiExternal/AddressDTO.java
@@ -1,15 +1,16 @@
 package com.edutech.backend.dtos.apiExternal;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotNull;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record AdressDTO(
-        String logradouro,
-        String bairro,
-        String localidade,
-        String complemento,
-        String numero,
-        String uf,
-        String cep
+public record AddressDTO(
+        @NotNull String logradouro,
+        @NotNull String bairro,
+        @NotNull String localidade,
+        @NotNull String complemento,
+        @NotNull String numero,
+        @NotNull String uf,
+        @NotNull String cep
 ) {
 }
