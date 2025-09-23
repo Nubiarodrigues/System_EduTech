a88b20f48d24d441755c16edbe199394b2d0d95a
Núbia Rodrigues <nubiarodrigues2223@gmail.com>
2025-09-23 14:59:31 -0300

    refactor(auth): reorganizando identação

--- src/contexts/auth/AuthProvider.jsx
+++ src/contexts/auth/AuthProvider.jsx
@@ -1,52 +1,51 @@
 import { useEffect, useState } from "react";
-import * as authService from '../../services/authService';
+import * as authService from "../../services/authService";
 import { AuthContext } from "./AuthContext";
 
 export default function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [isLoadingLoggedUser, setIsLoadingLoggedUser] = useState(true);
 
-    const [user, setUser] = useState(null);
-    const [isLoadingLoggedUser, setIsLoadingLoggedUser] = useState(true);
-
-    useEffect(() => {
-        authService.getUser()
-            .then(response => setUser(response.data))
-            .catch(err => {
-                if (err.response?.status === 401) {
-                    setUser(null);
-                } else {
-                    console.error("Erro ao buscar usuário", err);
-                }
-            })
-            .finally(() => setIsLoadingLoggedUser(false));
-    }, []);
-
-
-
-    async function sendLogin(credentials) {
-        try {
-            await authService.login(credentials);
-            const response = await authService.getUser();
-            setUser(response.data);
-            return true;
-        } catch (error) {
-            throw error;
+  useEffect(() => {
+    authService
+      .getUser()
+      .then((response) => setUser(response.data))
+      .catch((err) => {
+        if (err.response?.status === 401) {
+          setUser(null);
+        } else {
+          console.error("Erro ao buscar usuário", err);
         }
+      })
+      .finally(() => setIsLoadingLoggedUser(false));
+  }, []);
+
+  async function sendLogin(credentials) {
+    try {
+      await authService.login(credentials);
+      const response = await authService.getUser();
+      setUser(response.data);
+      return true;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async function logout() {
-        try {
-            await authService.logout();
-            setUser(null);
-            return true;
-        } catch (error) {
-            console.log("Erro ao deslogar", error);
-        }
+  async function logout() {
+    try {
+      await authService.logout();
+      setUser(null);
+      return true;
+    } catch (error) {
+      console.log("Erro ao deslogar", error);
     }
+  }
 
-
-    return (
-        <AuthContext.Provider value={{ user, isLoadingLoggedUser, sendLogin, logout }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider
+      value={{ user, isLoadingLoggedUser, sendLogin, logout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 }
