92e1cff7fcff9726f63f9c9ebc9d1394ca9051f8
Núbia Rodrigues <nubiarodrigues2223@gmail.com>
2025-09-25 09:23:10 -0300

    feat(hook): criar hook customizado para fazer requisição de busca para a entidade Classroom

---
+++ src/hooks/classroom/useClassroomFindAll.js
@@ -0,0 +1,26 @@
+import * as classroomService from "../../services/classroomService";
+import { useState } from "react";
+
+const useClassroomFindAll = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [classrooms, setClassrooms] = useState([]);
+
+  const findAll = async () => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await classroomService.findAll();
+      setClassrooms(response.data);
+    } catch (err) {
+      setError(err);
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return { findAll, loading, error, classrooms };
+};
+
+export default useClassroomFindAll;
