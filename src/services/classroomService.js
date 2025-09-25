d423615e52fc73e3e682a40548e72a3872c5923c
Núbia Rodrigues <nubiarodrigues2223@gmail.com>
2025-09-25 15:31:58 -0300

    feat(service): criar serviço para fazer a filtragem que será consumido no hook customizado

--- src/services/classroomService.js
+++ src/services/classroomService.js
@@ -3,5 +3,9 @@ import axios from "axios";
 const BASE_URL = "https://localhost:8443";
 
 export const findAll = () => {
-    return axios.get(`${BASE_URL}/classrooms`, { withCredentials: true });
+  return axios.get(`${BASE_URL}/classrooms`, { withCredentials: true });
+};
+
+export const findAllFiltered = (params) => {
+  return axios.get(`${BASE_URL}/classrooms?${params}`, { withCredentials: true });
 };
