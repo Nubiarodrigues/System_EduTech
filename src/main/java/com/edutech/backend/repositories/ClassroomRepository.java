7828efcfe37b5df51939324ba5356667e972cc1c
Núbia Rodrigues <nubiarodrigues2223@gmail.com>
2025-09-29 09:57:14 -0300

    feat(repository): estender Specification no repository de Classroom

--- src/main/java/com/edutech/backend/repositories/ClassroomRepository.java
+++ src/main/java/com/edutech/backend/repositories/ClassroomRepository.java
@@ -6,8 +6,9 @@ import com.edutech.backend.enuns.TeachingStage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.edutech.backend.entities.Classroom;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ClassroomRepository extends JpaRepository<Classroom, Long> {
+public interface ClassroomRepository extends JpaRepository<Classroom, Long>, JpaSpecificationExecutor<Classroom> {
 
 	List<Classroom> findByModality(TeachingStage modality);
 }
