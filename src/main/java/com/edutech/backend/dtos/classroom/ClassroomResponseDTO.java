dc788a49d7f9770453299c235058aeb269949c24
Núbia Rodrigues <nubiarodrigues2223@gmail.com>
2025-09-29 09:37:48 -0300

    feat(dto): expondo mais informações da turma no DTO de resposta

--- src/main/java/com/edutech/backend/dtos/classroom/ClassroomResponseDTO.java
+++ src/main/java/com/edutech/backend/dtos/classroom/ClassroomResponseDTO.java
@@ -4,6 +4,7 @@ import com.edutech.backend.dtos.discipline.DisciplineSimpleResponseDTO;
 import com.edutech.backend.dtos.schoolnotice.SchoolNoticesResponseDTO;
 import com.edutech.backend.dtos.student.StudentSimpleResponseDTO;
 import com.edutech.backend.entities.Classroom;
+import com.edutech.backend.enuns.Shift;
 
 import java.util.Collections;
 import java.util.List;
@@ -13,6 +14,9 @@ public record ClassroomResponseDTO(
 		Long id,
 		Integer series,
 		String identifierSeries,
+        Integer capacity,
+        Shift shift,
+        Integer schoolYear,
 		List<DisciplineSimpleResponseDTO> disciplines,
 		List<StudentSimpleResponseDTO> students,
 		String coordinatorName,
@@ -23,6 +27,9 @@ public record ClassroomResponseDTO(
 				classroom.getId(),
 				classroom.getSeries(),
 				classroom.getIdentifierSeries(),
+                classroom.getCapacity(),
+                classroom.getShift(),
+                classroom.getSchoolYear(),
 				classroom.getDisciplines() != null ? classroom.getDisciplines().stream().map(DisciplineSimpleResponseDTO::new).collect(Collectors.toList()) : Collections.emptyList(),
 				classroom.getStudents() != null ? classroom.getStudents().stream().map(StudentSimpleResponseDTO::new).collect(Collectors.toList()) : Collections.emptyList(),
 				classroom.getCoordinatorClass() != null ? classroom.getCoordinatorClass().getName() : null,
