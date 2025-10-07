94d79d38a708d44488346b7cc09e2298388ee52c
Núbia Rodrigues <nubiarodrigues2223@gmail.com>
2025-10-07 09:52:08 -0300

    style(constants): criar formatação de enuns para melhor serialização para a interface

---
+++ src/components/constants/FormattedEnums.jsx
@@ -0,0 +1,20 @@
+export const formattedEnums = {
+  shiftFormat: {
+    MANHA: "Manhã",
+    TARDE: "Tarde",
+    NOITE: "Noite",
+  },
+
+  stagesFormat: {
+    ENSINO_INFANTIL: "Ensino Infantil",
+    ENSINO_FUNDAMENTAL_I: "Ensino Fundamental I",
+    ENSINO_FUNDAMENTAL_II: "Ensino Fundamental II",
+    ENSINO_MEDIO: "Ensino Médio",
+    EDUCACAO_JOVENS_ADULTOS: "Educação de Jovens e Adultos",
+  },
+
+  typeSchoolFormat: {
+    PUBLICA: "Pública",
+    PRIVADA: "Privada",
+  },
+};
