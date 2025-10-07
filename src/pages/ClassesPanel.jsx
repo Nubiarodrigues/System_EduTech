2311b86236b433aebf057fc60bdff107b7084f90
Núbia Rodrigues <nubiarodrigues2223@gmail.com>
2025-10-07 12:54:03 -0300

    feat(page): aplicando a formatação de enuns e uso do Navigate

--- src/pages/ClassesPanel.jsx
+++ src/pages/ClassesPanel.jsx
@@ -4,19 +4,30 @@ import { Icon } from "@iconify/react";
 import BtnGeneric from "../components/buttons/BtnGeneric";
 import CircleBadge from "../components/indicators/CircleBadge";
 import useClassroomFiltered from "../hooks/classroom/useClassroomFiltered";
+import { useNavigate } from "react-router-dom";
+import { formattedEnums } from "../components/constants/FormattedEnums";
 
 function ClassesPanel() {
+  const navigate = useNavigate();
+
   const { findAll, loading, error, classrooms } = useClassroomFindAll();
-  const [filters, setFilters] = useState({ schoolYear:"", shift:"", modality:"" });
+
+  const [filters, setFilters] = useState({
+    schoolYear: "",
+    shift: "",
+    modality: "",
+  });
+
   const [appliedFilters, setAppliedFilters] = useState(null);
   const classroomsFiltered = useClassroomFiltered(appliedFilters);
+
 
   const handleFilterChange = (key, value) => {
     setFilters((prev) => ({ ...prev, [key]: value }));
   };
 
   const handleSearch = () => {
-    setAppliedFilters(filters); 
+    setAppliedFilters(filters);
   };
 
   useEffect(() => {
@@ -26,10 +37,8 @@ function ClassesPanel() {
   if (loading) return <p>Carregando...</p>;
   if (error) return <p>Erro: {error.message}</p>;
 
-  const formattedShift = {
-    MANHA: "Manhã",
-    TARDE: "Tarde",
-    NOITE: "Noite",
+  const handleAccessClass = (classroom) => {
+    navigate(`/classroom/${classroom.id}`);
   };
 
   return (
@@ -48,7 +57,13 @@ function ClassesPanel() {
       <div className="flex gap-20">
         <div className="flex flex-col p-4 items-center gap-2">
           <label for="year">Ano</label>
-          <select id="year" name="year" value={filters.schoolYear} className="rounded-sm border border-[#e1dcdc] p-2" onChange={(e) => handleFilterChange("schoolYear", e.target.value)}>
+          <select
+            id="year"
+            name="year"
+            value={filters.schoolYear}
+            className="rounded-sm border border-[#e1dcdc] p-2"
+            onChange={(e) => handleFilterChange("schoolYear", e.target.value)}
+          >
             <option value="">Selecione</option>
             <option value="2025">2025</option>
             <option value="2024">2024</option>
@@ -58,7 +73,13 @@ function ClassesPanel() {
 
         <div className="flex flex-col items-center justify-center gap-2">
           <label for="turn">Turno</label>
-          <select id="turn" name="turn" value={filters.shift} className="rounded-sm border border-[#e1dcdc] p-2" onChange={(e) => handleFilterChange("shift", e.target.value)}>
+          <select
+            id="turn"
+            name="turn"
+            value={filters.shift}
+            className="rounded-sm border border-[#e1dcdc] p-2"
+            onChange={(e) => handleFilterChange("shift", e.target.value)}
+          >
             <option value="">Selecione</option>
             <option value="MANHA">Manhã</option>
             <option value="TARDE">Tarde</option>
@@ -68,13 +89,21 @@ function ClassesPanel() {
 
         <div className="flex flex-col items-center justify-center gap-2">
           <label for="stage">Modalidade/Etapa</label>
-          <select id="stage" name="stage" value={filters.modality} className="rounded-sm border border-[#e1dcdc] p-2" onChange={(e) => handleFilterChange("modality", e.target.value)}>
+          <select
+            id="stage"
+            name="stage"
+            value={filters.modality}
+            className="rounded-sm border border-[#e1dcdc] p-2"
+            onChange={(e) => handleFilterChange("modality", e.target.value)}
+          >
             <option value="">Selecione</option>
             <option value="ENSINO_INFANTIL">Ensino Infantil</option>
             <option value="ENSINO_FUNDAMENTAL_I">Ensino Fundamental I</option>
             <option value="ENSINO_FUNDAMENTAL_II">Ensino Fundamental II</option>
             <option value="ENSINO_MEDIO">Ensino Médio</option>
-            <option value="EDUCACAO_JOVENS_ADULTOS">Educação de Jovens e Adultos</option>
+            <option value="EDUCACAO_JOVENS_ADULTOS">
+              Educação de Jovens e Adultos
+            </option>
           </select>
         </div>
       </div>
@@ -107,10 +136,10 @@ function ClassesPanel() {
 
           <div>
             {classroomsFiltered?.map((c) => (
-              <div key={c.id} className="p-2 flex justify-between border border-[#e1dcdc] hover:bg-[#DFE0DB] cursor-pointer">
+              <div key={c.id} className="p-2 flex justify-between border border-[#e1dcdc] hover:bg-[#DFE0DB] cursor-pointer" onClick={() => handleAccessClass(c)}>
                 <div>{c.schoolYear}</div>
                 <div>{c.id}</div>
-                <div>{formattedShift[c.shift]}</div>
+                <div>{formattedEnums.shiftFormat[c.shift]}</div>
                 <div>{c.identifierSeries}</div>
                 <div>{c.capacity}</div>
                 <div className="flex gap-2 items-center">
