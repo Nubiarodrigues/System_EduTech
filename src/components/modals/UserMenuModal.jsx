add037e2a47765d3c1e408b31d1f299960e67cee
Núbia Rodrigues <nubiarodrigues2223@gmail.com>
2025-10-07 09:49:34 -0300

    style(modals): estilizção de modal de perfil

--- src/components/modals/UserMenuModal.jsx
+++ src/components/modals/UserMenuModal.jsx
@@ -6,18 +6,25 @@ export function UserMenuModal({ isOpen, setOpen, name, role, registration }) {
   if (!isOpen) return null;
 
   return (
-    <div onClick={() => setOpen(false)}>
-      <div onClick={(e) => e.stopPropagation()}>
-        <div>
+    <>
+      <div className="fixed inset-0 bg-black opacity-50 z-40 transition-opacity duration-300" onClick={() => setOpen(false)}/>
+
+      <div className="fixed top-1/2 left-250 transform -translate-x-1/2 -translate-y-60  bg-gray-400 p-6 rounded shadow-lg z-50" onClick={(e) => e.stopPropagation()}>
+        <div className="flex flex-col gap-2">
           <p>Nome: {name}</p>
           <p>Permissão: {role}</p>
           <p>Matrícula: {registration}</p>
         </div>
-        <div>
-          <button>Sair</button>
-          <button onClick={() => navigate("/profile")}>Perfil</button>
+
+        <div className="flex gap-10 mt-4 bg-blue-200 justify-center">
+          <button className="bg-red-500 text-white px-4 py-2 rounded"  onClick={() => console.log("Logout")}>
+            Sair
+          </button>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={() => navigate("/profile")}>
+            Perfil
+          </button>
         </div>
       </div>
-    </div>
+    </>
   );
 }
