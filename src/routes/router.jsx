66b1b974aa5d76e53f990ed58c82017f1f79a5cc
Núbia Rodrigues <nubiarodrigues2223@gmail.com>
2025-10-07 13:03:54 -0300

    feat(routes): adiciona rotas /classes, /classroom/:id e /students/:idStudent

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -3,9 +3,11 @@ import AdminForm from "../components/forms/AdminForm";
 import SchoolForm from "../components/forms/SchoolForm";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
-import Login from "../pages/Login";;
+import Login from "../pages/Login";
 import SchoolPanel from "../pages/SchoolPanel";
 import ClassesPanel from "../pages/ClassesPanel";
+import ClassroomDetails from "../pages/ClassroomDetails";
+import StudentDetails from "../pages/StudentDetails";
 
 export const router = createBrowserRouter([
   {
@@ -14,7 +16,9 @@ export const router = createBrowserRouter([
     children: [
       { index: true, element: <Home /> },
       { path: "/school-details", element: <SchoolPanel /> },
-      {path: "classes", element: <ClassesPanel />}
+      { path: "/classes", element: <ClassesPanel /> },
+      { path: "/classroom/:id", element: <ClassroomDetails /> },
+      { path: "/students/:idStudent", element: <StudentDetails /> },
     ],
   },
 
